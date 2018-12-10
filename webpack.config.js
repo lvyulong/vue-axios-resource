20c7005807bd7b32df0c4b3ed46dd067afd3e3b6
吕玉龙 <lvyl@hrtps.com>
2018-12-10 19:40:07 +0800

    使用es6开发，开发完成后使用webpack打包，使用babel-loader编译

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+    mode:'development',
+    entry: path.resolve(__dirname, './src/index.js'),
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename: "index.js"
+    },
+    module:{
+        rules:[
+            // es6语法编译
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+        ]
+    }
+
+}
