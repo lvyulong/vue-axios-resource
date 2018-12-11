e2d3c33661c5ac3f573cfbff7eae0c6aa9afc3f6
吕玉龙 <lvyl@hrtps.com>
2018-12-11 08:48:26 +0800

    使用umd方式打包

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,14 @@
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -530,3 +540,4 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var babe
 /***/ })
 
 /******/ });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
     entry: path.resolve(__dirname, './src/index.js'),
     output:{
         path:path.resolve(__dirname,'dist'),
+        libraryTarget: "umd",
         filename: "index.js"
     },
     module:{
