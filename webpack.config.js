eac18089664ce3ea0bbe653b84f16542c60fecab
吕玉龙 <lvyl@hrtps.com>
2018-12-15 13:48:39 +0800

    打包压缩

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-    mode:'development',
+    mode:'production',
     entry: path.resolve(__dirname, './src/index.js'),
     output:{
         path:path.resolve(__dirname,'dist'),
