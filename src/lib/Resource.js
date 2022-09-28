5634203e932a7ec30fd50a6093b41da299dbf5dd
lvyl <lvy@hrtps.com>
2022-09-28 11:07:08 +0800

    将config改为_config，防止与外部变量冲突

--- src/lib/Resource.js
+++ src/lib/Resource.js
@@ -4,14 +4,14 @@ Resource类中所有的方法，都只接受一个option对象；
 option对象中的可配置项：body、params、headers等
 */
 
-function Resource(config) {
-    this.config = config;
+function Resource(_config) {
+    this._config = _config;
     // 如果有actions，生成这些额外的方法，
     //这些方法是实例的方法，并不是公有方法（原型链上的方法,也就是说每个实例的这些方法可能都不一样）
-    if (config.actions && config.actions.length > 0) {
-        for(var i=0;i<config.actions.length;i++){
+    if (_config.actions && _config.actions.length > 0) {
+        for(var i=0;i<_config.actions.length;i++){
             // 此处必须用let声明，如果用var在for循环中声明变量会有问题
-            let v = config.actions[i];
+            let v = _config.actions[i];
             // 如果有形如/:id的参数
             if(v.url.indexOf('/:')!=-1){
                 var names = v.url.split('/:');
@@ -26,7 +26,7 @@ function Resource(config) {
                         option.params[actionParam] = ''
                     }
                     var selfOption = {
-                        url: this.baseUrl + config.url + '/' + actionName + '/' + option.params[actionParam],
+                        url: this.baseUrl + _config.url + '/' + actionName + '/' + option.params[actionParam],
                         method: v.method
                     };
                     option = Object.assign({}, this.baseOption, selfOption, option);
@@ -35,7 +35,7 @@ function Resource(config) {
             } else {
                 this[v.name] = function (option) {
                     var selfOption = {
-                        url: this.baseUrl + config.url + '/' + v.url,
+                        url: this.baseUrl + _config.url + '/' + v.url,
                         method: v.method
                     };
                     option = Object.assign({}, this.baseOption, selfOption, option);
@@ -49,7 +49,7 @@ function Resource(config) {
 // index方法，注意传参顺序
 Resource.prototype.query = function (option) {
     var selfOption = {
-        url: this.baseUrl + this.config.url,
+        url: this.baseUrl + this._config.url,
         method: 'GET'
     };
     option = Object.assign({}, this.baseOption, selfOption, option);
@@ -58,7 +58,7 @@ Resource.prototype.query = function (option) {
 // view
 Resource.prototype.get = function (option) {
     var selfOption = {
-        url: this.baseUrl + this.config.url + '/' + option.params.id,
+        url: this.baseUrl + this._config.url + '/' + option.params.id,
         method: 'GET'
     };
     option = Object.assign({}, this.baseOption, selfOption, option);
@@ -68,7 +68,7 @@ Resource.prototype.get = function (option) {
 // create
 Resource.prototype.save = function (option) {
     var selfOption = {
-        url: this.baseUrl + this.config.url,
+        url: this.baseUrl + this._config.url,
         method: 'POST'
     };
     option = Object.assign({}, this.baseOption, selfOption, option);
@@ -79,7 +79,7 @@ Resource.prototype.save = function (option) {
 Resource.prototype.update = function (option) {
     var id = (option.params&&option.params.id) || (option.data&&option.data.id);
     var selfOption = {
-        url: this.baseUrl + this.config.url + '/' +  id,
+        url: this.baseUrl + this._config.url + '/' +  id,
         method: 'PUT'
     };
     option = Object.assign({}, this.baseOption, selfOption, option);
@@ -90,7 +90,7 @@ Resource.prototype.update = function (option) {
 Resource.prototype.delete = function (option) {
     var id = (option.params&&option.params.id) || (option.data&&option.data.id);
     var selfOption = {
-        url: this.baseUrl + this.config.url + '/' + id,
+        url: this.baseUrl + this._config.url + '/' + id,
         method: 'DELETE'
     };
     option = Object.assign({}, this.baseOption, selfOption, option);
